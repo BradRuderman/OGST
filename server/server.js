c57d244893e592ab468c6e4a1515aa128ef7f59a
Brad Ruderman <bradruderman@gmail.com>
2013-05-12 10:25:41 -0400

    Added server file

---
+++ server/server.js
@@ -0,0 +1,19 @@
+
+Meteor.methods({
+
+});
+
+Meteor.startup(function () {
+	if (Meteor.users.find().count() === 0) {
+	  Accounts.createUser({"email":"admin@test.com", "password":"test"});
+	}
+});
+
+Meteor.publish("projects", function () {
+    return Projects.find();
+});
+
+Meteor.publish("categories", function(){
+	return Categories.find();
+});
+
