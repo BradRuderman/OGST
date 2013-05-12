30b7c7f469dd0166c4e0bf927d73ab3d83e70aa8
Brad Ruderman <bradruderman@gmail.com>
2013-05-12 07:27:56 -0400

    Pushed intial code for admin view screen

--- client/project.js
+++ client/project.js
@@ -1,11 +1,12 @@
 
 /* Helpers */
 
-var updateItemLocation = function(mid,newCat,top,left){
+var updateItemLocation = function(mid,newCat,sort){
 	Categories.update(
 	   { _id: mid },
 	   {
-	     $set: { 'type': newCat, 'top': top, 'left': left},
+	     //$set: { 'type': newCat, 'top': top, 'left': left},
+	     $set: { 'type': newCat, 'sort': sort },
 	   }
 	)
 }
@@ -14,35 +15,69 @@ var getProjectsCat = function(catName){
 	return Categories.find( { "type": catName, "project" : Session.get("currentProject")._id }, {sort : {"sort" : 1} });
 }
 
-Meteor.subscribe("categories", function(){
-	Template.project.tatics = function(){
-		return getProjectsCat("Tatic");
-	}
+var subscribeCats =  function(){
+	return Meteor.subscribe("categories", function(){
+		Template.project.tatics = function(){
+			return getProjectsCat("Tatic");
+		}
 
-	Template.project.objectives = function(){
-		return getProjectsCat("Objective");
-	}
+		Template.project.objectives = function(){
+			return getProjectsCat("Objective");
+		}
 
-	Template.project.strategies = function(){
-		return getProjectsCat("Strategy");
-	}
+		Template.project.strategies = function(){
+			return getProjectsCat("Strategy");
+		}
 
-	Template.project.goals = function(){
-		return getProjectsCat("Goal");
-	}
+		Template.project.goals = function(){
+			return getProjectsCat("Goal");
+		}
 
-	Template.project.pending = function(){
-		return getProjectsCat("Pending");
-	}
-});
+		Template.project.pending = function(){
+			return getProjectsCat("Pending");
+		}
+	});
+}
+
+catSubscription = subscribeCats();
 
 Template.project.rendered = function(){
-	$('.item').droppable({
-		accept:".cat"
+	var newCat;
+	/*$('.item').draggable({
+		revert: "invalid",
+		stop: function(event,ui){
+			console.log(ui.offset);
+			console.log(ui.position);
+			console.log(newCat);
+			updateItemLocation($(this).attr("item_id"),newCat);
+			//$(this).css({"position":"absolute","x":ui.offset.x + "px","y":ui.offset.y +"px"});
+			//$('#' + newCat).append(ui.helper);
+		}
 	});
-	$('.item').draggable({
-		helper: "clone"
-	});
+	$('.cat').droppable({
+		accept:".item",
+		drop: function(event,ui){
+			newCat = $(this).attr("id");
+		}
+	});*/
+	$('.cat').sortable({
+      connectWith: ".cat",
+      stop: function(event,ui){
+      	catSubscription.stop();
+      	var item = $(ui.item);
+      	var newCat = item.parent().attr("id");
+      	$.each(item.parent().children(), function(i,val){
+      		var itemId = $(val).attr("item_id");
+      		console.log(newCat);
+      		console.log(i);
+      		console.log(itemId);
+      		updateItemLocation(itemId,newCat,i);
+      	});
+      	item.remove();
+      	catSubscription = subscribeCats();
+      }
+    });
+
 }
 
 /*stop: function( event, ui ) {
